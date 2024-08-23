061b1c8a80952c35512a543fd2f2f794d10032ad
Gabriel Sorrab <gabrieldebarros_@hotmail.com>
2024-08-23 19:59:19 -0300

    Add Hello World

---
+++ exemplosDeIf.java
@@ -0,0 +1,9 @@
+public class exemplosDeIf {
+    public static void main(String[] args) {
+        if(2 <= 3 && 3 == 4){
+            System.out.println("Certo");
+        }else if (1 == 1){
+            System.out.println("Errado");
+        }
+    }
+}
